59d7ff29f5b16d41cdb78672bd3a38da3a2a079c
LeonZ <leon_yy99@hotmail.com>
2019-12-26 18:06:37 +0800

    Add compression level select option
    Compression level allow user to send smaller file size and also the image size will keep

--- mods/canvas_send_image.js
+++ mods/canvas_send_image.js
@@ -1,26 +1,26 @@
 module.exports = {
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Name
 //
 // This is the name of the action displayed in the editor.
-//---------------------------------------------------------------------
+//———————————————————————
 
 name: "Canvas Send Image",
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Section
 //
 // This is the section the action will fall into.
-//---------------------------------------------------------------------
+//———————————————————————
 
 section: "Image Editing",
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Subtitle
 //
 // This function generates the subtitle displayed next to the name.
-//---------------------------------------------------------------------
+//———————————————————————
 
 subtitle: function(data) {
 	const channels = ['Same Channel', 'Command Author', 'Mentioned User', 'Mentioned Channel', 'Default Channel', 'Temp Variable', 'Server Variable', 'Global Variable'];
@@ -31,11 +31,11 @@ subtitle: function(data) {
 author: "LeonZ",
 version: "1.1.0",
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Storage Function
 //
 // Stores the relevant variable info for the editor.
-//---------------------------------------------------------------------
+//———————————————————————
 
 variableStorage: function(data, varType) {
 	const type = parseInt(data.storage2);
@@ -43,17 +43,17 @@ variableStorage: function(data, varType) {
 	return ([data.varName3, 'Message']);
 },
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Fields
 //
 // These are the fields for the action. These fields are customized
 // by creating elements with corresponding IDs in the HTML. These
 // are also the names of the fields stored in the action's JSON data.
-//---------------------------------------------------------------------
+//———————————————————————
 
-fields: ["storage", "varName", "channel", "varName2", "message", "Spoiler", "storage2", "varName3"],
+fields: ["storage", "varName", "channel", "varName2", "message", "compress", "spoiler", "storage2", "varName3"],
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Command HTML
 //
 // This function returns a string containing the HTML used for
@@ -67,7 +67,7 @@ fields: ["storage", "varName", "channel", "varName2", "message", "Spoiler", "sto
 // Each is an array: index 0 for commands, index 1 for events.
 // The names are: sendTargets, members, roles, channels, 
 //                messages, servers, variables
-//---------------------------------------------------------------------
+//———————————————————————
 
 html: function(isEvent, data) {
 	return `
@@ -100,11 +100,26 @@ html: function(isEvent, data) {
 	<textarea id="message" rows="2" placeholder="Insert message here..." style="width: 94%"></textarea>
 </div><br>
 <div style="padding-top: 8px;">
-	<div style="float: left; width: 94%;">
+	<div style="float: left; width: 44%;">
 		Image Spoiler:<br>
-		<select id="Spoiler" class="round">
+		<select id="spoiler" class="round">
 			<option value="0" selected>No</option>
 			<option value="1">Yes</option>
+		</select><br>
+	</div>
+	<div style="padding-left: 5%; float: left; width: 50%;">
+		Compression Level:<br>
+		<select id="compress" class="round">
+			<option value="0">1</option>
+			<option value="1">2</option>
+			<option value="2">3</option>
+			<option value="3">4</option>
+			<option value="4">5</option>
+			<option value="5">6</option>
+			<option value="6">7</option>
+			<option value="7">8</option>
+			<option value="8">9</option>
+			<option value="9" selected>10</option>
 		</select><br>
 	</div>
 </div><br><br>
@@ -122,13 +137,13 @@ html: function(isEvent, data) {
 </div>`
 },
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Editor Init Code
 //
 // When the HTML is first applied to the action editor, this code
 // is also run. This helps add modifications or setup reactionary
 // functions for the DOM elements.
-//---------------------------------------------------------------------
+//———————————————————————
 
 init: function() {
 	const {glob, document} = this;
@@ -138,13 +153,13 @@ init: function() {
 	glob.variableChange(document.getElementById('storage2'), 'varNameContainer3');
 },
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Bot Function
 //
 // This is the function for the action within the Bot's Action class.
 // Keep in mind event calls won't have access to the "msg" parameter, 
 // so be sure to provide checks for variable existance.
-//---------------------------------------------------------------------
+//———————————————————————
 
 action: function(cache) {
 	const Discord = require('discord.js');
@@ -160,6 +175,7 @@ action: function(cache) {
 	const channel = parseInt(data.channel);
 	const varName2 = this.evalMessage(data.varName2, cache);
 	const target = this.getSendTarget(channel, varName2, cache);	
+	let compress = parseInt(data.compress);
 	const image = new Canvas.Image();
 	image.src = imagedata;
 	const canvas = Canvas.createCanvas(image.width,image.height);
@@ -167,15 +183,15 @@ action: function(cache) {
 	ctx.drawImage(image, 0, 0, image.width, image.height)
 	var fs = require("fs");
 	let name;
-	const Spoiler = parseInt(data.Spoiler);
-	switch(Spoiler) {
+	const spoiler = parseInt(data.spoiler);
+	switch(spoiler) {
 		case 0:
 			name = 'image.png';
 			break;
 		case 1:
 			name = 'SPOILER_image.png';
 	}
-	const buffer = canvas.toBuffer('image/png');
+	const buffer = canvas.toBuffer('image/png',{compressionLevel:compress});
 	const attachment = new Discord.Attachment(buffer, name);
 	const _this = this;
 	if(target && target.send) {
@@ -190,14 +206,14 @@ action: function(cache) {
 	}
 },
 
-//---------------------------------------------------------------------
+//———————————————————————
 // Action Bot Mod
 //
 // Upon initialization of the bot, this code is run. Using the bot's
 // DBM namespace, one can add/modify existing functions if necessary.
 // In order to reduce conflictions between mods, be sure to alias
 // functions you wish to overwrite.
-//---------------------------------------------------------------------
+//———————————————————————
 
 mod: function(DBM) {
 }
